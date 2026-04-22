5b05cfbfd93cad82a129e110fc7fcfe3b46170c4
leticiasilva24 <leticia.silva24@estudante.ifto.edu.br>
2026-04-22 08:26:37 -0300

    criação tela de login

---
+++ login-controller.js
@@ -0,0 +1,28 @@
+function mostrarCadastro() {
+    document.getElementById('box-login').style.display = 'none';
+    document.getElementById('box-cadastro').style.display = 'block';
+}
+
+function mostrarLogin() {
+    document.getElementById('box-cadastro').style.display = 'none';
+    document.getElementById('box-login').style.display = 'block';
+}
+
+function entrarComoConvidado() {
+    localStorage.setItem('usuarioAtivo', JSON.stringify({ nome: 'Convidado', tipo: 'visitante' }));
+    alert("Entrando como convidado...");
+    window.location.href = 'dashboard.html'; // Mude para o nome da sua página principal
+}
+
+// Lógica de envio (Simulação)
+document.getElementById('form-login').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("Login realizado!");
+    window.location.href = 'dashboard.html';
+});
+
+document.getElementById('form-cadastro').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("Conta criada com sucesso!");
+    mostrarLogin();
+});
