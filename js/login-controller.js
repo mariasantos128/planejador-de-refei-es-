822cec5e779a96011f433cfdbf07fd64c5881026
leticiasilva24 <leticia.silva@estudante.ifto.edu.br>
2026-04-29 10:23:13 -0300

    a

--- js/login-controller.js
+++ js/login-controller.js
@@ -11,14 +11,16 @@ function mostrarLogin() {
 function entrarComoConvidado() {
     localStorage.setItem('usuarioAtivo', JSON.stringify({ nome: 'Convidado', tipo: 'visitante' }));
     alert("Entrando como convidado...");
-   window.location.href = 'html/usuarios.html';
+    window.location.href = 'html/dashboard.html';
 }
 
 // Lógica de envio (Simulação)
 document.getElementById('form-login').addEventListener('submit', (e) => {
     e.preventDefault();
+    const nomeUsuario = document.getElementById('login-email').value.split('@')[0]; // Pega o nome antes do @
+    localStorage.setItem('nomeUsuario', nomeUsuario); // Salva o nome no localStorage
     alert("Login realizado!");
-   window.location.href = 'html/usuarios.html';
+    window.location.href = 'html/dashboard.html';
 });
 
 document.getElementById('form-cadastro').addEventListener('submit', (e) => {
